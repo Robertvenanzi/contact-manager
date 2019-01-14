a9515c2ca2937ad60a36383fb8381ab6830b5f64
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-14 00:15:35 -0800

    created state for contacts and mapp through contacts

---
+++ src/components/Contacts.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+
+class Contacts extends Component {
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Doe',
+                email: 'Jdoe@gmail.com',
+                phone: '555-555-5555'
+            },
+            {
+                id: 2,
+                name: 'Karen Williams',
+                email: 'karen@gmail.com',
+                phone: '123-123-1234'
+            },
+            {
+                id: 3,
+                name: 'Henry Johnson',
+                email: 'henry@gmail.com',
+                phone: '111-111-1111'
+            }
+        ]
+    };
+
+    render() {
+        const { contacts } = this.state;
+        return (
+            <React.Fragment>
+                {contacts.map(contact => (
+                    <Contact
+                        key={contact.id}
+                        contact={contact}
+                    />
+                ))}
+            </React.Fragment>
+        )
+    }
+}
+
+export default Contacts;
