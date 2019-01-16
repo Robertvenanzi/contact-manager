dfc02d2d94d2ade012a758aaf07c3059a4b0dd77
Robert Venanzi <37943093+Robertvenanzi@users.noreply.github.com>
2019-01-16 15:25:07 -0800

    updated our api get request to use async/await

--- src/context.js
+++ src/context.js
@@ -24,15 +24,13 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
 
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(res => this.setState({ contacts: res.data }));
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    this.setState({ contacts: res.data });
   }
 
   render() {
